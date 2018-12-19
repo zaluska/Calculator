90282812a27af475ffc6cf18e6f3941b22055ece
Nataliia Zaluska <nataliya.zaluskaya@gmail.com>
2018-12-19 14:31:25 +0100

    Adding unit tests that uses random input values. Testing positive and negative values.

--- src/test/java/Calculator_NataliiaZ/CalculatorTests.java
+++ src/test/java/Calculator_NataliiaZ/CalculatorTests.java
@@ -1,166 +1,243 @@
 package Calculator_NataliiaZ;
 
+import java.util.Random;
+
 import org.junit.Test;
 
 import Calculator_NataliiaZ.Implementations.Calculator;
 import junit.framework.TestCase;
 
 public class CalculatorTests extends TestCase {
+
 	@Test
 	public void testAddition() {
 		// Arrange
-		double firstNumber = 2.0;
-		double secondNumber = 2.0;
-		double expected = 4.0;
-		
+		double firstNumber = 0.0;
+		double secondNumber = 0.0;
+		double expected = 0.0;
+		double actual = 0.0;
+
+		Random rnd = new Random();
 		Calculator calculator = new Calculator();
-		
+
 		// Act
-		double actual = calculator.addition(firstNumber, secondNumber);
-		
-		// Assert
-		assertEquals(expected, actual);
+		for (int i = 0; i < 50; i++) {
+			firstNumber = rnd.nextDouble() * 100 - 50;
+			secondNumber = rnd.nextDouble() * 100 - 50;
+			expected = firstNumber + secondNumber;
+
+			actual = calculator.addition(firstNumber, secondNumber);
+
+			// Assert
+			assertEquals(expected, actual);
+		}
 	}
-	
+
 	@Test
 	public void testSubtraction() {
 		// Arrange
-		double firstNumber = 3.0;
-		double secondNumber = 2.0;
-		double expected = 1.0;
-		
+		double firstNumber = 0.0;
+		double secondNumber = 0.0;
+		double expected = 0.0;
+		double actual = 0.0;
+
+		Random rnd = new Random();
 		Calculator calculator = new Calculator();
-		
+
 		// Act
-		double actual = calculator.subtraction(firstNumber, secondNumber);
-		
-		// Assert
-		assertEquals(expected, actual);
+		for (int i = 0; i < 50; i++) {
+			firstNumber = rnd.nextDouble() * 100 - 50;
+			secondNumber = rnd.nextDouble() * 100 - 50;
+			expected = firstNumber - secondNumber;
+
+			actual = calculator.subtraction(firstNumber, secondNumber);
+
+			// Assert
+			assertEquals(expected, actual);
+		}
 	}
-	
+
 	@Test
 	public void testMultiplication() {
 		// Arrange
-		double firstNumber = 3.0;
-		double secondNumber = 2.0;
-		double expected = 6.0;
-		
+		double firstNumber = 0.0;
+		double secondNumber = 0.0;
+		double expected = 0.0;
+		double actual = 0.0;
+
+		Random rnd = new Random();
 		Calculator calculator = new Calculator();
-		
+
 		// Act
-		double actual = calculator.multiplication(firstNumber, secondNumber);
-		
-		// Assert
-		assertEquals(expected, actual);
+		for (int i = 0; i < 50; i++) {
+			firstNumber = rnd.nextDouble() * 100 - 50;
+			secondNumber = rnd.nextDouble() * 100 - 50;
+			expected = firstNumber * secondNumber;
+
+			actual = calculator.multiplication(firstNumber, secondNumber);
+
+			// Assert
+			assertEquals(expected, actual);
+		}
 	}
-	
+
 	@Test
 	public void testDivision() {
 		// Arrange
-		double firstNumber = 3.0;
-		double secondNumber = 2.0;
-		double expected = 1.5;
-		
+		double firstNumber = 0.0;
+		double secondNumber = 0.0;
+		double expected = 0.0;
+		double actual = 0.0;
+
+		Random rnd = new Random();
 		Calculator calculator = new Calculator();
-		
+
 		// Act
-		double actual = calculator.division(firstNumber, secondNumber);
-		
-		// Assert
-		assertEquals(expected, actual);
+		for (int i = 0; i < 50; i++) {
+			firstNumber = rnd.nextDouble() * 100 - 50;
+			secondNumber = rnd.nextDouble() * 100 - 50;
+			expected = firstNumber / secondNumber;
+
+			actual = calculator.division(firstNumber, secondNumber);
+
+			// Assert
+			assertEquals(expected, actual);
+		}
 	}
-	
+
 	@Test
 	public void testProcent() {
 		// Arrange
-		double firstNumber = 100.0;
-		double secondNumber = 10.0;
-		double expected = 10.0;
-		
+		double firstNumber = 0.0;
+		double secondNumber = 0.0;
+		double expected = 0.0;
+		double actual = 0.0;
+
+		Random rnd = new Random();
 		Calculator calculator = new Calculator();
-		
+
 		// Act
-		double actual = calculator.procent(firstNumber, secondNumber);
-		
-		// Assert
-		assertEquals(expected, actual);
+		for (int i = 0; i < 50; i++) {
+			firstNumber = rnd.nextDouble() * 100 - 50;
+			secondNumber = rnd.nextDouble() * 100 - 50;
+			expected = firstNumber * secondNumber / 100;
+
+			actual = calculator.procent(firstNumber, secondNumber);
+
+			// Assert
+			assertEquals(expected, actual);
+		}
 	}
-	
+
 	@Test
 	public void testRnd() {
 		// Arrange
-		double firstNumber = 11.0;
-		double secondNumber = 81.0;
-		
+		double firstNumber = 0.0;
+		double secondNumber = 0.0;
+		double expected = 0.0;
+		double actual = 0.0;
+
+		Random rnd = new Random();
 		Calculator calculator = new Calculator();
-		
+
 		// Act
-		double result = calculator.rnd(firstNumber, secondNumber);
-		
-		// Assert
-		assertTrue(result >= firstNumber && result <= secondNumber);
+		for (int i = 0; i < 50; i++) {
+			firstNumber = rnd.nextDouble() * 100 - 50;
+			secondNumber = rnd.nextDouble() * 100 - 50;
+			expected = Math.random() * (secondNumber - firstNumber) + firstNumber;
+
+			actual = calculator.rnd(firstNumber, secondNumber);
+
+			// Assert
+			assertEquals(expected, actual);
+		}
 	}
-	
+
 	@Test
-	public void testSqrt () {
+	public void testSqrt() {
 		// Arrange
-		double firstNumber = 25.0;
-		double expected = 5.0;
-		
+		double firstNumber = 0.0;
+		double expected = 0.0;
+		double actual = 0.0;
+
+		Random rnd = new Random();
 		Calculator calculator = new Calculator();
-		
+
 		// Act
-		double actual = calculator.sqrt(firstNumber);
-		
-		// Assert
-		assertEquals(expected, actual);
+		for (int i = 0; i < 50; i++) {
+			firstNumber = rnd.nextDouble() * 100 - 50;
+			expected = Math.sqrt(firstNumber);
+
+			actual = calculator.sqrt(firstNumber);
+
+			// Assert
+			assertEquals(expected, actual);
+		}
 	}
-	
+
 	@Test
-	public void testPower2 () {
+	public void testPower2() {
 		// Arrange
-		double firstNumber = 3.0;
-		double expected = 9.0;
-		
+		double firstNumber = 0.0;
+		double expected = 0.0;
+		double actual = 0.0;
+
+		Random rnd = new Random();
 		Calculator calculator = new Calculator();
-		
+
 		// Act
-		double actual = calculator.power2(firstNumber);
-		
-		// Assert
-		assertEquals(expected, actual);
+		for (int i = 0; i < 50; i++) {
+			firstNumber = rnd.nextDouble() * 100 - 50;
+			expected = Math.pow(firstNumber, 2);
+
+			actual = calculator.power2(firstNumber);
+
+			// Assert
+			assertEquals(expected, actual);
+		}
 	}
-	
+
 	@Test
-	public void testPower3 () {
+	public void testPower3() {
 		// Arrange
-		double firstNumber = 3.0;
-		double expected = 27.0;
-		
+		double firstNumber = 0.0;
+		double expected = 0.0;
+		double actual = 0.0;
+
+		Random rnd = new Random();
 		Calculator calculator = new Calculator();
-		
+
 		// Act
-		double actual = calculator.power3(firstNumber);
-		
-		// Assert
-		assertEquals(expected, actual);
+		for (int i = 0; i < 50; i++) {
+			firstNumber = rnd.nextDouble() * 100 - 50;
+			expected = Math.pow(firstNumber, 3);
+
+			actual = calculator.power3(firstNumber);
+
+			// Assert
+			assertEquals(expected, actual);
+		}
 	}
-	
+
 	@Test
-	public void testAbs () {
+	public void testAbs() {
 		// Arrange
-		double firstNumber = -3.0;
-		double expected = 3.0;
-		
+		double firstNumber = 0.0;
+		double expected = 0.0;
+		double actual = 0.0;
+
+		Random rnd = new Random();
 		Calculator calculator = new Calculator();
-		
+
 		// Act
-		double actual = calculator.abs(firstNumber);
-		
-		// Assert
-		assertEquals(expected, actual);
+		for (int i = 0; i < 50; i++) {
+			firstNumber = rnd.nextDouble() * 100 - 50;
+			expected = Math.abs(firstNumber);
+
+			actual = calculator.abs(firstNumber);
+
+			// Assert
+			assertEquals(expected, actual);
+		}
 	}
 }
-
-
-
